7dba9fe8ec10cbf3184d100f755050072c78b4e4
Samir Enrique Taborda Sierra <stabordasierra@gmail.com>
2022-04-12 09:22:53 -0500

    en el foodReducer se agrego una funcion para traer un alimento seleccionado en la lista de estos y se agregaron algunos estilos

--- src/store/foodReducer.js
+++ src/store/foodReducer.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import { CHANGE_FOODLABEL, CHANGE_FOODNAME, CHANGE_FOODPRICE } from './restaurantReducer'
 
 const FOODS_LOADING = 'FOODS_LOADING'
 const FOODS_SUCCESS = 'FOODS_SUCCESS'
 const FOODS_ERROR = 'FOODS_ERROR'
 const FOODS_FINISH = 'FOODS_FINISH'
+const FOOD_SUCCESS = 'FOOD_SUCCESS'
 
 export function getFoods(restaurantId){
   return async function(dispatch){
@@ -23,8 +25,36 @@ export function getFoods(restaurantId){
   }
 }
 
+export function getFood(foodId){
+  return async function(dispatch){
+    dispatch({ type: FOODS_LOADING })
+    try {
+      const token = localStorage.getItem("token")
+      const { data } = await axios({
+        method: 'GET',
+        baseURL: process.env.REACT_APP_SERVER_URL,
+        url: `/foods/getFood/${foodId}`,
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      })
+      dispatch({ type: FOOD_SUCCESS, payload: data})
+      dispatch({ type: CHANGE_FOODNAME, payload: data.foodName})
+      dispatch({ type: CHANGE_FOODLABEL, payload: data.foodLabel})
+      dispatch({ type: CHANGE_FOODPRICE, payload: data.foodPrice})
+      console.log(data)
+    } catch(error){
+      dispatch({ type: FOODS_ERROR, payload: error})
+    } finally {
+      dispatch({ type: FOODS_FINISH})
+    }
+  }
+}
+
+
 const initialState = {
   foods: [],
+  food: {},
   loading: false,
   error: null,
 }
@@ -40,6 +70,11 @@ export function foodReducer(state = initialState, action){
       return {
         ...state,
         foods: action.payload,
+      }
+    case FOOD_SUCCESS:
+      return {
+        ...state,
+        food: action.payload,
       }
     case FOODS_ERROR:
       return {
